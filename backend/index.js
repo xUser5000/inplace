71b98a4828caadaad34b0662be2eb06025b404c3
xUser5000 <abdallahar1974@gmail.com>
2023-11-02 01:15:02 +0200

    chore: use sequelize and refactor backend skeleton

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,15 @@
 const express = require("express");
-const pool = require("./db");
+const sequelize = require("./core/db");
+
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Hello World\n");
 });
 
-async function main() {
-    await pool.query("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR(255))");
-
-    app.listen(3000, () => {
-        console.log("Server is running on port 3000");
-    });
-}
-
-main();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, async () => {
+    await sequelize.authenticate();
+    console.log("Database connection established");
+    console.log(`Server is running on port ${PORT}`);
+});

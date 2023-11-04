938ed8a69fc70d0efba70e6cc375bd5b178ffaa4
xUser5000 <abdallahar1974@gmail.com>
2023-11-04 17:35:42 +0200

    feat: api errors & error handling & request schema validation

---
+++ backend/core/middlewares.js
@@ -0,0 +1,43 @@
+const joi = require("joi");
+const { APIError, ValidationError } = require("./errors");
+
+const errorHandler = () => {
+    return (err, req, res, next) => {
+        if (err instanceof APIError) {
+            return res.status(err.code).json({
+                name: err.name,
+                message: err.message,
+                details: err.details
+            });
+        }
+
+        console.error(err);
+        res.sendStatus(500);
+    }
+};
+
+const schemaValidator = (schema) => {
+    return (req, res, next) => {
+        const result = schema.validate(req.body, { presence: "required" });
+        if (result.error) {
+            const errors = result.error.details.reduce((acc, error) => {
+                console.log(error);
+                const field = error.context.label;
+                const message = error.message;
+            
+                if (!acc[field]) {
+                  acc[field] = [];
+                }
+            
+                acc[field].push(message);
+                return acc;
+            }, {});
+
+            throw new ValidationError("Bad request body", errors);
+        }
+
+        next();
+    };
+};
+
+module.exports = { errorHandler, schemaValidator };
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,25 @@
+// Libraries
 const express = require("express");
+const morgan = require("morgan");
+const joi = require("joi");
+require('express-async-errors');
+
+// Custom modules
 const sequelize = require("./core/db");
+const { errorHandler, schemaValidator } = require("./core/middlewares");
+const { NotFoundError } = require("./core/errors");
 
 const app = express();
 
+// Middlewares
+app.use(morgan("dev"));
+
 app.get("/", (req, res) => {
-    res.send("Hello World\n");
+    res.send("Welcome to Inplace");
 });
+
+// Error handling middleware
+app.use(errorHandler());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {

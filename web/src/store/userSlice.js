c7343c9baf89ad9036edc279d4535bc9612e788e
omarashrafdev <omar.ashraf.dev@gmail.com>
2024-02-05 08:52:52 +0200

    Arabic translation for login page - needs fix - initial commit

--- web/src/store/userSlice.js
+++ web/src/store/userSlice.js
@@ -1,42 +1,42 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { useTranslation } from 'react-i18next';
 
-export const loginUser = createAsyncThunk(
-    'user/loginUser',
-    async(userCredentials)=>{
-        // TODO: Change login api provided by backend
-        const request = await axios.get(`...`, userCredentials);
-        const response = await request.data.data;
-        console.log(response);
-        localStorage.setItem('user', JSON.stringify(response));
-        return response;
-    }
-);
+const { t } = useTranslation();
+
+export const loginUser = createAsyncThunk('user/loginUser', async (userCredentials) => {
+  // TODO: Change login api provided by backend
+  const request = await axios.get(`...`, userCredentials);
+  const response = await request.data.data;
+  console.log(response);
+  localStorage.setItem('user', JSON.stringify(response));
+  return response;
+});
 
 const userSlice = createSlice({
-    name: 'user',
-    initialState: {
-        loading: false,
-        user: null,
-        error: null
-    },
-    extraReducers:(builder) => {
-        builder.addCase(loginUser.pending, (state)=>{
-            state.loading = true;
-            state.user = null;
-            state.error = null;
-        })
-        .addCase(loginUser.fulfilled, (state, action) => {
-            state.loading = false;
-            state.user = null;
-            console.log(action.error.message)
-            if (action.error.message === 'Request failed with status code 401') {
-                state.error = 'Access Denied! Invalid Credentials';
-            }
-            else {
-                state.error = action.error.message;
-            }
-        })
-    }
+  name: 'user',
+  initialState: {
+    loading: false,
+    user: null,
+    error: null
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUser.pending, (state) => {
+        state.loading = true;
+        state.user = null;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = null;
+        console.log(action.error.message);
+        if (action.error.message === 'Request failed with status code 401') {
+          state.error = t('Access Denied! Invalid Credentials');
+        } else {
+          state.error = action.error.message;
+        }
+      });
+  }
 });
 
 export default userSlice.reducer;

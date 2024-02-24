25689af7f56b5b7015120f268ea54bb86a0abb50
xUser5000 <abdallahar1974@gmail.com>
2024-02-24 12:31:14 +0200

    fix(profile): null first and last name

--- backend/users/routes.js
+++ backend/users/routes.js
@@ -2,11 +2,14 @@ const { User } = require("./models");
 const userRouter = require("express").Router();
 const joi = require("joi");
 const { defineRoute } = require("../core/define_route");
+
 const FEATURE = "users";
+
 const updateProfileSchema = joi.object({
 	first_name: joi.string(),
 	last_name: joi.string()
 });
+
 defineRoute({
 	router: userRouter,
 	feature: FEATURE,
@@ -15,15 +18,20 @@ defineRoute({
 	description: "Update the user first_name or last_name or both",
 	inputSchema: updateProfileSchema,
 	handler: async (req, res) => {
-		await User.update(
-			{ first_name: req.body.first_name, last_name: req.body.last_name },
-			{
-				where: {
-					id: req.userId
-				}
-			}
-		);
-		res.json("ok");
+		const user = await User.findByPk(req.userId);
+
+		if (req.body.first_name) {
+			user.first_name = req.body.first_name;
+		}
+
+		if (req.body.last_name) {
+			user.last_name = req.body.last_name;
+		}
+
+		user.save();
+
+		res.json(user);
 	}
 });
+
 module.exports = { userRouter };

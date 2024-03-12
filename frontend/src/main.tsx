e33547a925b89c2fe0efee1ecf155de547770622
Mohamed Elhefni <elhefnymohamed446@gmail.com>
2024-03-12 22:57:56 +0200

    feat(register): add register page with basic form structure and validation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Layout } from './pages/layout.tsx'
 import { Login } from './pages/login.tsx'
+import { Register } from './pages/register.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         >
           <Route index element={<App />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
         </Route>
 
       </Routes>

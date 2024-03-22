89c70cba21d11a1f78802a64b56351a9ad06476d
omarashrafdev <omar.ashraf.dev@gmail.com>
2024-03-23 00:43:33 +0200

    register button onSubmit function creates user and redirects to login

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,9 +1,8 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
     Form,
     FormControl,
@@ -11,25 +10,40 @@ import {
     FormItem,
     FormLabel,
     FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Link } from "react-router-dom"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Link } from "react-router-dom";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "@/components/providers/auth-provider";
+import { useEffect, useState } from "react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
-const registerFormSchema: any = z.object({
-    firstName: z.string().min(2),
-    lastName: z.string().min(2),
-    email: z.string().email(),
-    password: z.string().min(8),
-    passwordConfirmation: z.string().min(8),
-}).refine(data => data.password === data.passwordConfirmation, {
-    message: "Password and password confirmation must match",
-    path: ["passwordConfirmation"],
-})
-
+const registerFormSchema: any = z
+    .object({
+        firstName: z.string().min(2),
+        lastName: z.string().min(2),
+        email: z.string().email(),
+        password: z.string().min(8),
+        passwordConfirmation: z.string().min(8),
+    })
+    .refine((data) => data.password === data.passwordConfirmation, {
+        message: "Password and password confirmation must match",
+        path: ["passwordConfirmation"],
+    });
 
 export function Register() {
+    const navigate = useNavigate();
+    const { user } = useAuth();
+
+    useEffect(() => {
+        if (user) {
+            return navigate("/");
+        }
+    }, [user]);
+
+    const [registerErr, setRegisterErr] = useState<string | null>(null);
     const form = useForm<z.infer<typeof registerFormSchema>>({
         resolver: zodResolver(registerFormSchema),
         defaultValues: {
@@ -39,10 +53,33 @@ export function Register() {
             password: "",
             passwordConfirmation: "",
         },
-    })
+    });
 
     function onSubmit(values: z.infer<typeof registerFormSchema>) {
-        console.log(values)
+        setRegisterErr(null);
+        fetch(import.meta.env.VITE_API_URL + "/auth/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                first_name: values.firstName,
+                last_name: values.lastName,
+                email: values.email,
+                password: values.password,
+                confirm_password: values.passwordConfirmation,
+            }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.message) {
+                    setRegisterErr(data.message);
+                }
+
+                if (data.email) {
+                    return navigate("/login");
+                }
+            });
     }
 
     return (
@@ -53,8 +90,10 @@ export function Register() {
 
             <CardContent>
                 <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-4"
+                    >
                         <div className="flex itesm-center justify-between gap-5">
                             <FormField
                                 control={form.control}
@@ -63,7 +102,11 @@ export function Register() {
                                     <FormItem>
                                         <FormLabel>First Name</FormLabel>
                                         <FormControl>
-                                            <Input type="text" placeholder="First Name" {...field} />
+                                            <Input
+                                                type="text"
+                                                placeholder="First Name"
+                                                {...field}
+                                            />
                                         </FormControl>
                                         <FormMessage />
                                     </FormItem>
@@ -77,14 +120,16 @@ export function Register() {
                                     <FormItem>
                                         <FormLabel>Last Name</FormLabel>
                                         <FormControl>
-                                            <Input type="text" placeholder="Last Name" {...field} />
+                                            <Input
+                                                type="text"
+                                                placeholder="Last Name"
+                                                {...field}
+                                            />
                                         </FormControl>
                                         <FormMessage />
                                     </FormItem>
                                 )}
                             />
-
-
                         </div>
 
                         <FormField
@@ -94,7 +139,11 @@ export function Register() {
                                 <FormItem>
                                     <FormLabel>Email</FormLabel>
                                     <FormControl>
-                                        <Input type="email" placeholder="email" {...field} />
+                                        <Input
+                                            type="email"
+                                            placeholder="email"
+                                            {...field}
+                                        />
                                     </FormControl>
                                     <FormMessage />
                                 </FormItem>
@@ -107,7 +156,11 @@ export function Register() {
                                 <FormItem>
                                     <FormLabel>Password</FormLabel>
                                     <FormControl>
-                                        <Input type="password" placeholder="password" {...field} />
+                                        <Input
+                                            type="password"
+                                            placeholder="password"
+                                            {...field}
+                                        />
                                     </FormControl>
                                     <FormMessage />
                                 </FormItem>
@@ -120,27 +173,37 @@ export function Register() {
                                 <FormItem>
                                     <FormLabel>Password Confirmation</FormLabel>
                                     <FormControl>
-                                        <Input type="password" placeholder="Password Confirmation" {...field} />
+                                        <Input
+                                            type="password"
+                                            placeholder="Password Confirmation"
+                                            {...field}
+                                        />
                                     </FormControl>
                                     <FormMessage />
                                 </FormItem>
                             )}
                         />
-                        <Button className="w-full" type="submit">Register</Button>
+
+                        {registerErr && (
+                            <Alert variant="destructive">
+                                <AlertTitle>Error</AlertTitle>
+                                <AlertDescription>
+                                    {registerErr}
+                                </AlertDescription>
+                            </Alert>
+                        )}
+
+                        <Button className="w-full" type="submit">
+                            Register
+                        </Button>
                         <div className="flex items-center justify-between mb-1">
                             <Label asChild>
                                 <Link to="/login">Have Account ?</Link>
                             </Label>
                         </div>
-
-
                     </form>
                 </Form>
-
             </CardContent>
-
         </Card>
-    )
+    );
 }
-
-

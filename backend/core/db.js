236b01c4ea433aff5840def6599f2bd873099ab0
xUser5000 <abdallahar1974@gmail.com>
2024-06-30 09:28:52 +0300

    fix: require ssl mode for pg in production

--- backend/core/db.js
+++ backend/core/db.js
@@ -1,12 +1,19 @@
 const { Sequelize } = require("sequelize");
 
+const requireSSL = process.env.NODE_ENV == "production";
+
 const sequelize = new Sequelize({
 	dialect: "postgres",
 	host: process.env.DB_HOST,
 	port: process.env.DB_PORT,
 	database: process.env.DB_NAME,
 	username: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD
+	password: process.env.DB_PASSWORD,
+	dialectOptions: {
+		ssl: {
+			require: requireSSL
+		}
+	}
 });
 
 module.exports = sequelize;
